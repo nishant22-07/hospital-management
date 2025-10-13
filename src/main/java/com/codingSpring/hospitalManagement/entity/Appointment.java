2461e906e8c6871573a3d318ada5d61adccfa3f0
nishant <nishantkudale04@gmail.com>
2025-10-14 01:26:36 +0530

    created appointment entity and established many to one relationship with patient entity

---
+++ src/main/java/com/codingSpring/hospitalManagement/entity/Appointment.java
@@ -0,0 +1,31 @@
+package com.codingSpring.hospitalManagement.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// Auto-increament
+    private Long id;
+
+    @Column(name= "appointment_time",nullable = false)
+    private LocalDateTime appointmentTime;
+
+    @Column(length = 500)
+    private String reason;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+}
