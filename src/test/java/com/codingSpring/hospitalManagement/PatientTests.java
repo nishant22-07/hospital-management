2661cd98dec7b32219ac9d5fe157daf16e20451d
nishant <nishantkudale04@gmail.com>
2025-10-14 00:13:51 +0530

    tested jpql queries

--- src/test/java/com/codingSpring/hospitalManagement/PatientTests.java
+++ src/test/java/com/codingSpring/hospitalManagement/PatientTests.java
@@ -1,14 +1,17 @@
 package com.codingSpring.hospitalManagement;
 
+import com.codingSpring.hospitalManagement.dto.BloodGroupCountResponseEntity;
 import com.codingSpring.hospitalManagement.entity.Patient;
 import com.codingSpring.hospitalManagement.repository.PatientRepository;
 import com.codingSpring.hospitalManagement.service.PatientService;
-import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @SpringBootTest
@@ -34,9 +37,43 @@ public class PatientTests {
     public  void testTransactionMethods(){
 //        Patient patient = patientService.getPatientById(1L);
 //        Patient patient = patientRepository.findById(1L).orElseThrow(() -> new EntityNotFoundException("Patient not "));
-        Patient patient = patientRepository.findByName("Nishant Kumar");
 
-        System.out.println(patient);
+//        Patient patient = patientRepository.findByName("Nishant Kumar");
+
+//        List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(2000,05,12),"" +
+//                "nishant@example.com");
+
+
+//        List<Patient> patientList = patientRepository.findByBirthDateBetween(LocalDate.of(1999,01,01),LocalDate.of(2001,01,01));
+
+//        List<Patient> patientList = patientRepository.findByNameContainingOrderByIdDesc("i");
+
+//        List<Patient> patientList = patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
+
+//        List<Patient> patientList = patientRepository.findBornAfterDate(LocalDate.of(2000,01,01));
+//
+//        for(Patient patient : patientList) {
+//            System.out.println(patient);
+//        }
+//
+
+        //Projection
+        List<BloodGroupCountResponseEntity> bloodGroupList = patientRepository.findPatientCountGroupedByBloodGroup();
+        for(BloodGroupCountResponseEntity objects: bloodGroupList){
+            System.out.println(objects);
+        }
+//
+
+
+        int updateName = patientRepository.updateNameWithId("nishant Kudale" , 2L);
+        System.out.println(updateName);
+
+        Page<Patient> allPatients = patientRepository.findAllPatient(PageRequest.of(0,2,Sort.by("name")));
+        for(Patient patient:allPatients ){
+            System.out.println(patient);
+        }
+
+
     }
 
 }
