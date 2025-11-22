24a1e97c8b3061e3b9eab941158ea022e382ee49
nishant <nishantkudale04@gmail.com>
2025-11-22 16:26:27 +0530

    created department entity with one-to-one relationship to doctor and many-to-many relationship with doctors list

---
+++ src/main/java/com/codingSpring/hospitalManagement/entity/Department.java
@@ -0,0 +1,63 @@
+package com.codingSpring.hospitalManagement.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100,nullable = false)
+    private String name;
+
+    @OneToOne
+    private Doctor doctor;
+
+    @ManyToMany
+    @JoinTable(
+            name = "my_dpt_doctors",
+            joinColumns = @JoinColumn(name = "dpt_id"),
+            inverseJoinColumns = @JoinColumn(name = "doctor_id")
+    )
+    private Set<Doctor> doctorsList = new HashSet<>();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
