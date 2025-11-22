c2ce920d3079d54ed6ae803d2da23ad28ba436eb
nishant <nishantkudale04@gmail.com>
2025-11-22 16:26:07 +0530

    added many to one relationship with doctor in appointment entity

--- src/main/java/com/codingSpring/hospitalManagement/entity/Appointment.java
+++ src/main/java/com/codingSpring/hospitalManagement/entity/Appointment.java
@@ -28,4 +28,8 @@ public class Appointment {
     @ManyToOne
     @JoinColumn(name = "patient_id",nullable = false)
     private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Doctor doctor;
 }
